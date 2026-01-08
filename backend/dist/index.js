97ded62df0a80c35b0dedeaa7788017e8ff70c57
Sivakumar <sivakumarr3105@gmail.com>
2026-01-08 18:52:21 +0530

    feat:msg is sent through ws

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,16 +1,43 @@
 import { WebSocketServer, WebSocket } from "ws";
+import crypto from "crypto";
 const wss = new WebSocketServer({ port: 8080 });
-let allsockets = [];
+let userconnections = [];
 wss.on("connection", (socket) => {
-    allsockets.push(socket);
-    let socketId = "user" + Math.random();
     socket.onmessage = (e) => {
-        console.log("message", e.data);
-        setTimeout(() => {
-            allsockets.forEach((s) => {
-                s.send(e.data);
-            });
-        }, 1000);
+        try {
+            const message = e.data.toString();
+            const data = JSON.parse(message);
+            if (data.type === "join") {
+                const user = {
+                    userid: crypto.randomUUID(),
+                    socket: socket,
+                    username: data.username
+                };
+                userconnections.push(user);
+                console.log(userconnections);
+            }
+            if (data.type === "message") {
+                const sender = userconnections.find((user) => user.socket === socket);
+                if (!sender)
+                    return;
+                userconnections.forEach((connection) => {
+                    connection.socket?.send(JSON.stringify({
+                        type: "message",
+                        message: data.message,
+                        username: sender.username
+                    }));
+                    console.log({
+                        type: "message",
+                        message: data.message,
+                        username: sender.username
+                    });
+                });
+            }
+        }
+        catch (e) {
+            console.error(e);
+            socket.send(JSON.stringify({ type: "error", message: "Invalid message" }));
+        }
     };
 });
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,48 @@
 import {WebSocketServer,WebSocket} from "ws";
-
+import crypto from "crypto";
 const wss=new WebSocketServer({port:8080});
 
+interface usersocket{
+    userid:string,
+    socket?:WebSocket,
+    username:string
+}
 
-let allsockets:WebSocket[]=[];
+let userconnections:usersocket[]=[];
 wss.on("connection",(socket)=>{
-    allsockets.push(socket);
-    let socketId="user"+Math.random()
     socket.onmessage=(e)=>{
-        console.log("message",e.data)
-        setTimeout(()=>{
-            allsockets.forEach((s)=>{
-                s.send(e.data)
+        try{
+        const message=e.data.toString();
+        const data=JSON.parse(message);
+        if(data.type==="join"){
+            const user:usersocket={
+                userid:crypto.randomUUID(),
+                socket:socket,
+                username:data.username
+            }
+            userconnections.push(user);
+            console.log(userconnections);
+        }
+        if(data.type==="message"){
+            const sender=userconnections.find((user)=> user.socket===socket);
+            if(!sender) return ;
+            userconnections.forEach((connection)=>{
+                connection.socket?.send(JSON.stringify({
+                    type:"message",
+                    message:data.message,
+                    username:sender.username
+                }))
+                console.log({
+                    type:"message",
+                    message:data.message,
+                    username:sender.username
+                });
             })
-
-        },1000)
+        }
+    }catch(e){
+        console.error(e);
+        socket.send(JSON.stringify({type:"error",message:"Invalid message"}));  
+    }
     }
 })
 
